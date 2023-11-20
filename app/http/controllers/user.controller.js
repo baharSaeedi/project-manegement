b2a473304f14325955c44354d03b295f3e4df207
baharSaeedi <b.saeedi.ha@gmail.com>
2023-11-20 23:13:42 +0330

    add edit profile api

--- app/http/controllers/user.controller.js
+++ app/http/controllers/user.controller.js
@@ -10,6 +10,34 @@ class User {
       next(error);
     }
   }
+  async editProfile(req, res, next) {
+    try {
+      const profileData = req.body;
+      const userId = req.user.userId;
+      let fields = ["firstName", "lastName", "skills"];
+      let wrongValues = ["", " ", undefined, NaN, null, 0, -1];
+      Object.entries(profileData).forEach(([key, value]) => {
+        if (!fields.includes(key)) delete profileData[key];
+        if (wrongValues.includes(value)) delete profileData[key];
+      });
+
+      const editedProfile = await userModel.updateOne(
+        { _id: userId },
+        { $set: profileData }
+      );
+
+      if (editedProfile.modifiedCount > 0) {
+        return res.status(200).json({
+          status: 200,
+          message: "profile updated",
+        });
+      }
+
+      return res.status(400).json({ message: "something went wrong!" });
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = {
--- app/router/user.js
+++ app/router/user.js
@@ -5,6 +5,7 @@ const { UserController } = require("../http/controllers/user.controller");
 const router = Router();
 
 router.get("/profile", checkAuth, UserController.getProfile);
+router.get("/profile", checkAuth, UserController.editProfile);
 
 module.exports = {
   userRoutes: router,
