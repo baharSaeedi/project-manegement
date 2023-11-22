ffbc0e2c446c41699860d514aab38a611dc3a385
baharSaeedi <b.saeedi.ha@gmail.com>
2023-11-22 23:36:09 +0330

    tag validation

--- app/http/validations/project.js
+++ app/http/validations/project.js
@@ -3,6 +3,9 @@ const { body } = require("express-validator");
 function createProjectValidator() {
   return [
     body("title").notEmpty().withMessage("title can not be empty"),
+    body("tags")
+      .isArray({ min: 0, max: 10 })
+      .withMessage("tags should be array with length between 0 - 10 "),
     body("text").notEmpty().withMessage("text can not be empty"),
   ];
 }
